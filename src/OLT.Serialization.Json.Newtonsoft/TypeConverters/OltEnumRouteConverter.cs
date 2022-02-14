323fd79f813befc16452363128a0d50a72ba776d
Chris Straw <chrisstraw@users.noreply.github.com>
2022-02-14 17:33:44 -0500

    Initial Solution (#1)

---
+++ src/OLT.Serialization.Json.Newtonsoft/TypeConverters/OltEnumRouteConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace OLT.Serialization.Json.Newtonsoft
+{
+
+    /// <summary>
+    /// Serialization of <see cref="System.Enum"/> in route parameters 
+    /// 
+    /// <see href="https://github.com/dotnet/aspnetcore/issues/4008">Code copied from</see>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>    
+    /// <exception cref="JsonSerializationException"><see cref="System.Runtime.Serialization.EnumMemberAttribute"/> is not present on the value</exception>
+    public class OltEnumRouteConverter<T> : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var val = value as string;
+            if (string.IsNullOrWhiteSpace(val))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<T>(@"""" + value.ToString() + @"""");
+        }
+    }
+}
